88cbcee1d5d6ef731f0fdb7837400dadecce14ea
idol <joyce@idolconsulting.co.za>
2022-03-28 21:48:53 +0200

    Commit

---
+++ TicketApp/src/main/java/com/mycompany/TicketApp/user/User.java
@@ -0,0 +1,55 @@
+package com.mycompany.TicketApp.user;
+
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String confirmPassword;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
---
+++ TicketApp/src/test/java/com/mycompany/TicketApp/UserRepositoryTests.java
@@ -0,0 +1,31 @@
+package com.mycompany.TicketApp;
+
+import com.mycompany.TicketApp.user.User;
+import com.mycompany.TicketApp.user.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+
+    @Autowired private UserRepository repo;
+
+    @Test
+    public void testAddNew() {
+        User user = new User();
+        user.setUsername("nokubonga@gmail.com");
+        user.setPassword("test");
+        user.setConfirmPassword("test");
+
+        User savedUser = repo.save(user);
+
+
+    }
+
+}
