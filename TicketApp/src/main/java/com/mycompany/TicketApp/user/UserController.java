c3a63ef1efd4efa0fe5fe598020ff14da815694d
idol <joyce@idolconsulting.co.za>
2022-03-31 08:57:59 +0200

    added files

--- TicketApp/src/main/java/com/mycompany/TicketApp/user/UserController.java
+++ TicketApp/src/main/java/com/mycompany/TicketApp/user/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UserController {
@@ -19,6 +21,12 @@ public class UserController {
         return "register";
     }
 
+    @PostMapping("/register/save")
+    public String saveUser(User user, RedirectAttributes ra) {
+        service.save(user);
+        ra.addFlashAttribute("message","The user has been saved successfully");
+        return "redirect:/register";
+    }
 
     @GetMapping("/ticket")
     public  String showTicketPage(Model model) {
